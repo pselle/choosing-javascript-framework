3505729ff6a02f032c522e74fb84560fa73fe38b
pselle <pamela.selle@gmail.com>
2014-03-17 20:44:35 -0400

    Update index to table view

--- ember/js/models/property.js
+++ ember/js/models/property.js
@@ -1,26 +1,37 @@
 // Without Ember Data
 // App.Property = Ember.Object.extend({});
 
+// With Ember Data (to leverage fixtures)
 App.Property = DS.Model.extend({
-    address: DS.attr('string')
+    address: DS.attr('string'),
+    zip: DS.attr('number'),
+    price: DS.attr('number')
 });
 
 //todo
 App.Property.FIXTURES = [
     {
         id: 1,
-        address: '1123 Sunny Road'
+        address: '1123 Sunny Road',
+        zip: 37890,
+        price: 230000
     },
     {
         id: 2,
-        address: '409 Lavender Blvd'
+        address: '409 Lavender Blvd',
+        zip: 19106,
+        price: 120000
     },
     {
         id: 3,
-        address: '347 Lotus Lane'
+        address: '347 Lotus Lane',
+        zip: 10001,
+        price: 160000
     },
     {
         id: 4,
-        address: '23 Daisy Parkway'
+        address: '23 Daisy Parkway',
+        zip: 40502,
+        price: 1000000
     }
 ];
