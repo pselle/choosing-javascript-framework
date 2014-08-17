ed41df4b3d036e192aab70a9435c6ee05416ef08
Tim Ruffles <oi@truffles.me.uk>
2014-08-17 17:25:37 +0100

    clean

--- backbone/js/views.js
+++ backbone/js/views.js
@@ -1,20 +1,3 @@
-var StageView = Backbone.View.extend({
-    switchView: function(view) {
-        if(this.view) {
-            this.view.$el.fadeOut(150, switchOver.bind(this));
-        } else {
-            switchOver.call(this);
-        }
-        function switchOver() {
-            if(this.view) this.view.$el.remove();
-            this.view = view;
-            this.view.render();
-            this.view.$el.hide().fadeIn(150);
-            this.$el.append(this.view.el);
-        }
-    }
-});
-
 var PropertyShowView = Backbone.View.extend({
     tagName: 'div',
     className: 'property',
@@ -100,6 +83,22 @@ var PropertyListView = CollectionView.extend({
     ItemView: PropertyListItem
 });
 
+var StageView = Backbone.View.extend({
+    switchView: function(view) {
+        if(this.view) {
+            this.view.$el.fadeOut(150, this._doSwitch.bind(this, view));
+        } else {
+            this._doSwitch(view);
+        }
+    },
+    _doSwitch: function(view) {
+        if(this.view) this.view.$el.remove();
+        this.view = view;
+        this.view.render();
+        this.view.$el.hide().fadeIn(150);
+        this.$el.append(this.view.el);
+    }
+});
 
 function getTemplate(name) {
     return _.template(document.getElementById(name).innerHTML);
