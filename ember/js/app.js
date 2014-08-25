e3acf4090d1f6354bb102d83dc43b2a80dac6a69
Jamie White <jamie@jgwhite.co.uk>
2014-08-25 14:29:23 +0100

    [Ember] Add ApplicationRoute

--- ember/js/app.js
+++ ember/js/app.js
@@ -1,1 +1,11 @@
 var App = Ember.Application.create();
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return [{
+      streetAddress: '123 Foo Bar Avenue',
+      zipCode: '01011',
+      price: 1000000
+    }];
+  }
+});
