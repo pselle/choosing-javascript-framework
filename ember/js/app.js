9ac975505e9c84cff211ab047984b53d1e63f68f
Jamie White <jamie@jgwhite.co.uk>
2014-08-25 15:30:40 +0100

    [Ember] Fetch data with getJSON

--- ember/js/app.js
+++ ember/js/app.js
@@ -2,10 +2,6 @@ var App = Ember.Application.create();
 
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
-    return [{
-      streetAddress: '123 Foo Bar Avenue',
-      zipCode: '01011',
-      price: 1000000
-    }];
+    return $.getJSON('/shared/data.json');
   }
 });
