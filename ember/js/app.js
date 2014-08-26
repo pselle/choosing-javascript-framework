59babe17d47eaacfa22ed107f547bf58bee34103
Jamie White <jamie@jgwhite.co.uk>
2014-08-26 06:21:29 +0100

    [Ember] Add format number

--- ember/js/app.js
+++ ember/js/app.js
@@ -5,3 +5,7 @@ App.ApplicationRoute = Ember.Route.extend({
     return $.getJSON('/shared/data.json');
   }
 });
+
+Ember.Handlebars.helper('format-number', function(number, format) {
+  return numeral(number).format(format);
+});
