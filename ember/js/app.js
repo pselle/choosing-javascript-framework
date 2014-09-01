da72875fd167327f1dc4d33181909b818e717f83
Jamie White <jamie@jgwhite.co.uk>
2014-09-01 07:14:36 +0100

    [Ember] Add property route

--- ember/js/app.js
+++ ember/js/app.js
@@ -1,8 +1,22 @@
 var App = Ember.Application.create();
 
+App.Router.map(function() {
+  this.resource('property', { path: 'properties/:property_id' });
+});
+
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
     return $.getJSON('/shared/data.json');
+  }
+});
+
+App.PropertyRoute = Ember.Route.extend({
+  model: function(params) {
+    var id         = parseInt(params.property_id, 10);
+    var properties = this.modelFor('application');
+    var property   = properties.findBy('id', id);
+
+    return property;
   }
 });
 
